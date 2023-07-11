770a3345aa68aedc9b8c208c137165f74fea9eef
Matthew Ragan <raganmd@gmail.com>
2023-07-11 10:16:23 -0700

    Final URL Update

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://sudomagiccode.github.io/SudoSignals',
+  url: 'https://docs.sudosignals.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
