10ff904380fbe16647634cbf7282af702cb4c7f1
Matthew Ragan <raganmd@gmail.com>
2023-11-02 13:42:10 -0700

    Upgrades to v3 of docusaurus

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -1,8 +1,7 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+import { themes as prismThemes } from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
@@ -115,8 +114,9 @@ const config = {
         copyright: `Copyright © ${new Date().getFullYear()} SudoMagic, LLC.`,
       },
       prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
+        theme: prismThemes.oneLight,
+        darkTheme: prismThemes.dracula,
+        additionalLanguages: ['python', 'glsl'],
       },
     }),
 };
